5331ad096326d8f4431f53cffc5e95546fae7d70
Balázs Rigó <rigo.balazs86@gmail.com>
2020-06-30 14:17:49 +0200

    Lesson 7 Implemented!

--- BattleshipLiteApp/BattleshipLite/Program.cs
+++ BattleshipLiteApp/BattleshipLite/Program.cs
@@ -2,6 +2,7 @@
 using BattleshipLiteLibrary.Models;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -15,12 +16,101 @@ namespace BattleshipLite
         {
             WelcomeMessage();
 
-            PlayerInfoModel player1 = Createplayer("Player 1");
-            PlayerInfoModel player2 = Createplayer("Player 2");
+            PlayerInfoModel activePlayer = Createplayer("Player 1");
+            PlayerInfoModel opponent = Createplayer("Player 2");
+            PlayerInfoModel winner = null;
 
+            do
+            {
+                DisplayShotGrid(activePlayer);
 
+                RecordPlayerShot(activePlayer,opponent);
+
+                bool doesGameContinue = GameLogic.PlayerStillActive(opponent);
+
+                if (doesGameContinue == true)
+                {
+                    //Swap position
+                    (activePlayer, opponent) = (opponent, activePlayer);
+                }
+                else
+                {
+                    winner = activePlayer;
+                }                
+            } while (winner == null);
+
+            IdentifyWinner(winner);
 
             Console.ReadLine();
+        }
+
+        private static void IdentifyWinner(PlayerInfoModel winner)
+        {
+            Console.WriteLine($"Congratulations to {winner.UsersName} for winning!");
+            Console.WriteLine($"{winner.UsersName} took {GameLogic.GetShotCount(winner)} shots.");
+        }
+
+        private static void RecordPlayerShot(PlayerInfoModel activePlayer, PlayerInfoModel opponent)
+        {
+            bool isValidShot = false;
+            string row = "";
+            int column = 0;
+
+            do
+            {
+                string shot = AskForShot();
+                (row, column) = GameLogic.SplitShotIntoRowAndColumn(shot);
+                isValidShot = GameLogic.ValidateShot();
+
+                if (isValidShot == false)
+                {
+                    Console.WriteLine("Invalid Shot Location. Please try again.");
+                }
+
+            } while (isValidShot == false);
+
+            bool isAHit = GameLogic.IdentifyShotResult(opponent,row,column);
+
+            GameLogic.MarkShotResult(activePlayer,row,column,isAHit);
+        }
+
+        private static string AskForShot()
+        {
+            Console.WriteLine("Please enter your shot selection: ");
+            string output = Console.ReadLine();
+
+            return output;
+        }
+
+        private static void DisplayShotGrid(PlayerInfoModel activePlayer)
+        {
+            string currentRow = activePlayer.ShotGrid[0].SpotLetter;
+
+            foreach (var gridSpot in activePlayer.ShotGrid)
+            {
+                if (gridSpot.SpotLetter != currentRow)
+                {
+                    Console.WriteLine();
+                    currentRow = gridSpot.SpotLetter;
+                }
+
+                if (gridSpot.Status == GridSpotStatus.Empty)
+                {
+                    Console.Write($"{gridSpot.SpotLetter}{gridSpot.SpotNumber}");
+                }
+                else if (gridSpot.Status == GridSpotStatus.Hit)
+                {
+                    Console.Write(" X ");
+                }
+                else if (gridSpot.Status == GridSpotStatus.Miss)
+                {
+                    Console.Write(" O ");
+                }
+                else
+                {
+                    Console.Write(" ? ");
+                }
+            }
         }
 
         private static void WelcomeMessage()
